a1d213e95de3e7b6b9dcd83f76a0fd6d7d825ab2
SwaroopaLakshmiKavuri <77829593+SwaroopaLakshmiKavuri@users.noreply.github.com>
2021-02-08 09:24:07 +0530

    Add files via upload

---
+++ Countries.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Countries.aspx.cs" Inherits="WebAppCountries.Countries" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            <br />
+        </p>
+        <p>
+            Country&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlCountry" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlCountry_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+        <p>
+            State&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlState" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlState_SelectedIndexChanged">
+            </asp:DropDownList>
+        </p>
+        <div>
+            City&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="ddlCity" runat="server" AutoPostBack="True">
+            </asp:DropDownList>
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ Countries.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebAppCountries
+{
+    public partial class Countries : System.Web.UI.Page
+    {
+        SqlConnection cn = new SqlConnection("Data Source=DESKTOP-6IDKG38;Initial Catalog=WFA3DotNet;Integrated Security=True");
+        public void Bind_ddlCountry()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from Country", cn);
+            SqlDataReader dr = cmd.ExecuteReader();
+            ddlCountry.DataSource = dr;
+            ddlCountry.Items.Clear();
+            
+            ddlCountry.DataTextField = "Cname";
+            ddlCountry.DataValueField = "Cid";
+            ddlCountry.DataBind();
+            cn.Close();
+        }
+        public void Bind_ddlState()
+        {
+            cn.Open();
+
+            SqlCommand cmd = new SqlCommand("select Sid,Sname from State where Cid='" + ddlCountry.SelectedValue + "'", cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            ddlState.DataSource = dr;
+            ddlState.Items.Clear();
+            ddlState.Items.Add("--Please Select state--");
+            ddlState.DataTextField = "Sname";
+            ddlState.DataValueField = "Sid";
+            ddlState.DataBind();
+            cn.Close();
+        }
+        public void Bind_ddlCity()
+        {
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select CityId,CityName from City where Sid ='" + ddlState.SelectedValue + "'" , cn);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            ddlCity.DataSource = dr;
+            ddlCity.Items.Clear();
+            ddlCity.DataTextField = "CityName";
+            ddlCity.DataValueField = "CityID";
+            ddlCity.DataBind();
+            cn.Close();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Bind_ddlCountry();
+            }
+
+        }
+
+        protected void ddlCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bind_ddlState();
+        }
+
+        protected void ddlState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Bind_ddlCity();
+        }
+    }
+}
